dc22c134ab4882ae3337f2dfae372b7fbd9586a1
andre faria <andre.faria@ceiia.com>
2024-07-07 19:07:23 +0100

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,8 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   
-  socket.write('HTTP/1.0 200 OK\r\n' +
-        '\r\n')
+  socket.write('HTTP/1.0 200 OK\r\n\r\n')
   
   socket.on("close", () => {
     socket.end();
