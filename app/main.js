5d813c8512f124ebdfc61a3057e72dccb9edd1a5
andre faria <andre.faria@ceiia.com>
2024-07-07 19:13:22 +0100

    pass stage

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ const server = net.createServer((socket) => {
   socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
-    // server.close();
+    server.close();
   });
 });
 server.listen(4221, "localhost");
