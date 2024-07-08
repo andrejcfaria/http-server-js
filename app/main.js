79eafed251265bbc13c4dd85e2bdc55873271526
andre faria <fariaoandre@gmail.com>
2024-07-08 16:42:05 +0100

    chore: format

--- app/main.js
+++ app/main.js
@@ -10,21 +10,17 @@ const server = net.createServer((socket) => {
     const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
 
     const id = path.split('/')[2];
- 
 
-    if(path === "/") { 
-      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    if (path === '/') {
+      socket.write('HTTP/1.1 200 OK\r\n\r\n');
     } else if (id) {
-         const l = id.length;
+      const l = id.length;
       socket.write(
-      `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${l}\r\n\r\n${id}`
-    )
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${l}\r\n\r\n${id}`
+      );
     } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
     }
-
-
-    
   });
 });
 server.listen(4221, 'localhost');
