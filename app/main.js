6694baef93088fe6ad8a1b9b218dafa88b173083
andre faria <andre.faria@ceiia.com>
2024-07-08 09:07:00 +0100

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,25 +1,20 @@
-const net = require("net");
+const net = require('net');
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
+  socket.write('HTTP/1.1 200 OK\r\n\r\n');
+  socket.on('close', () => {
     socket.end();
     server.close();
   });
 
-  socket.on("data", data => {
-    const [m, path, proto] = data.toString().split('\r\n')[0].split(" ")
-console.log(path)
-    if(path === '/'){
-
-       socket.write('HTTP/1.1 200 OK\r\n\r\n');
-
-    } 
-      
-      else {
-         socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-
+  socket.on('data', (data) => {
+    const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
+    console.log(path);
+    if (path === '/') {
+      socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    } else {
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
     }
-  })
+  });
 });
-server.listen(4221, "localhost");
+server.listen(4221, 'localhost');
