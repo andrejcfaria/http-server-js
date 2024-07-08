e728b75551c6d1068c71c054cca29e83c232b0cc
andre faria <fariaoandre@gmail.com>
2024-07-08 22:11:24 +0100

    chore

--- app/main.js
+++ app/main.js
@@ -7,16 +7,19 @@ const server = net.createServer((socket) => {
   });
 
   socket.on('data', (data) => {
+    console.log(data.toString())
     const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
+    const userAgent = data.toString().split('\r\n')[3].split(":")[1]
+    console.log("userAgent : ",userAgent.split(':'))
 
     const id = path.split('/')[2];
 
     if (path === '/') {
       socket.write('HTTP/1.1 200 OK\r\n\r\n');
     } else if (id) {
-      const l = id.length;
+      const l = userAgent.length;
       socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${l}\r\n\r\n${id}`
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${l}\r\n\r\n${userAgent[1]}`
       );
     } else {
       socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
