64b181b327f7ac5b0663c8283193d33bec69f78e
andre faria <andre.faria@ceiia.com>
2024-07-08 16:18:57 +0100

    chore

--- app/main.js
+++ app/main.js
@@ -9,11 +9,11 @@ const server = net.createServer((socket) => {
 
   socket.on('data', (data) => {
     const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
-   console.log(path)
+
     const id = path.split("/")[2]
-   console.log(id)
+
    const l = (id.length)
-   console.log(l)
+
   
     
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${l}\r\n\r\n${id}`);
