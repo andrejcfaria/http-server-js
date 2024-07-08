4610da6b3432d2b960fb0d61185e3e41bc8e2477
andre faria <andre.faria@ceiia.com>
2024-07-08 16:36:17 +0100

    chore

--- app/main.js
+++ app/main.js
@@ -10,12 +10,19 @@ const server = net.createServer((socket) => {
     const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
 
     const id = path.split('/')[2]
-    console.log("id", id)
-    const l = id.length;
 
-    socket.write(
+    if(path === "/") { 
+      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    } else if (id) {
+      socket.write(
       `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${l}\r\n\r\n${id}`
-    );
+    )
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    }
+
+
+    
   });
 });
 server.listen(4221, 'localhost');
