c3177d4e5421fb18b312863d49054e665808460c
andre faria <andre.faria@ceiia.com>
2024-07-08 16:09:51 +0100

    fix

--- app/main.js
+++ app/main.js
@@ -10,10 +10,10 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
    const id = path.split("/")[1]
-   console.log(id.length - 1 )
+   console.log(id.length)
   
     
-      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${id.length}\r\n\r\n${id}`);
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${id.length + 1}\r\n\r\n${id}`);
    
    
   });
