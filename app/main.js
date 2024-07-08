5c380f9fa5d53bc7fa9952b0ecf25a99bd0f7b0e
andre faria <andre.faria@ceiia.com>
2024-07-08 16:37:37 +0100

    feat: test

--- app/main.js
+++ app/main.js
@@ -9,7 +9,8 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
 
-    const id = path.split('/')[2]
+    const id = path.split('/')[2];
+    const l = id.length;
 
     if(path === "/") { 
       socket.write("HTTP/1.1 200 OK\r\n\r\n")
