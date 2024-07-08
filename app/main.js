ba525720d1ac16b8b668a3c951279dcddb9a1849
andre faria <andre.faria@ceiia.com>
2024-07-08 16:38:08 +0100

    chore

--- app/main.js
+++ app/main.js
@@ -10,11 +10,12 @@ const server = net.createServer((socket) => {
     const [m, path, proto] = data.toString().split('\r\n')[0].split(' ');
 
     const id = path.split('/')[2];
-    const l = id.length;
+ 
 
     if(path === "/") { 
       socket.write("HTTP/1.1 200 OK\r\n\r\n")
     } else if (id) {
+         const l = id.length;
       socket.write(
       `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${l}\r\n\r\n${id}`
     )
