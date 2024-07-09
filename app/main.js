e438d5fedeb2316aaf677f8cc86265a4eb723d5b
andre faria <fariaoandre@gmail.com>
2024-07-09 08:58:38 +0100

    pass stage

--- app/main.js
+++ app/main.js
@@ -12,33 +12,23 @@ const server = net.createServer((socket) => {
 
     const [requestLine, ...headerLines] = requestData.split('\r\n');
     const [method, path, protocol] = requestLine.split(' ');
-
-    // Parse headers into an object
-    const headers = {};
-    headerLines.forEach(line => {
-      const [key, value] = line.split(': ');
-      if (key && value) {
-        headers[key] = value;
+    console.log(headerLines);
+    // const a = headerLines.split(",").map(([k,v]) => (
+    //   console.log(k,v)
+    // ))
+    let h = {};
+    headerLines.forEach((line) => {
+      const [k, v] = line.split(': ');
+      if (k && v) {
+        return (h[k] = v);
       }
     });
 
-    // Get User-Agent header
-    const userAgent = headers['User-Agent'];
+    const userAgent = h['User-Agent'];
 
-    // Log User-Agent for debugging
-    console.log('userAgent', userAgent);
-
-    if (userAgent) {
-      const contentLength = userAgent.length;
-
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${userAgent}`
-      );
-    } else {
-      // Handle the case where User-Agent header is missing
-      socket.write(
-"HTTP/1.1 200 OK\r\n\r\n"
-      );
+    if (method === 'GET' && path === '/user-agent') {
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}
+`);
     }
   });
 });
