8cf49ec5ff141f912946aebaf0b26c93109d1051
andre faria <fariaoandre@gmail.com>
2024-07-11 12:36:43 +0100

    chore

--- app/main.js
+++ app/main.js
@@ -21,10 +21,9 @@
       
       
       const [requestLine, ...headerLines] = requestData.split('\r\n');
-      const [method, path, protocol] = requestLine.split(' ');
+      const [method, reqPath, protocol] = requestLine.split(' ');
       
     
-      console.log(headerLines)
       let h = {};
       headerLines.forEach((line) => {
         const [k, v] = line.split(': ');
@@ -33,21 +32,22 @@
         }
       });
 
-      if (method === 'GET' && path === '/user-agent') {
+      if (method === 'GET' && reqPath === '/user-agent') {
         const userAgent = h['User-Agent'];
         socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}
   `);
       } else if (
         method === 'GET' &&
-        path !== '/user-agent' &&
-        path.includes('echo')
+        reqPath !== '/user-agent' &&
+        reqPath.includes('echo')
       ) {
         const body = path.split('/')[2];
         socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}
   `);
-      } else if (method === 'GET' && path.startsWith("/files/")) {
+      } else if (method === 'GET' && reqPath.startsWith("/files/")) {
+        console.log("first")
         const directory = process.argv[3];
-      const filename = path.split("/files/")[1];
+      const filename = reqPath.split("/files/")[1];
 
         // const filePath = p.join('/tmp', path.replace("/files", ""));
    
@@ -61,12 +61,12 @@
           if(file) {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`);
           } else {
-            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
           }
         })
             }
       } else {
-        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       }
     });
   });
