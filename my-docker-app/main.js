7a3b2772215c3cb7c7c66224f35ad4c7985fd234
andre faria <fariaoandre@gmail.com>
2024-07-11 15:26:28 +0100

    chore

--- my-docker-app/main.js
+++ my-docker-app/main.js
@@ -1,45 +1,75 @@
 const fs = require("fs");
 const net = require("net");
+const path = require("path");
+
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+
   socket.on("data", (data) => {
     const req = data.toString();
     console.log(req);
-    const path = req.split(" ")[1];
-    if (path === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    // else if (path === "/user-agent") {
-    else if (path.startsWith("/files/")) {
-      const directory = process.argv[3];
-      const filename = path.split("/files/")[1];
-      if (fs.existsSync(`${directory}/${filename}`)) {
-        const content = fs.readFileSync(`${directory}/${filename}`).toString();
-        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+    const requestPath = req.split(" ")[1];
+
+    if (requestPath === "/") {
+      fs.readFile("/home/andrefaria/48ed43726f7cdf54/my-docker-app/app/index.html", (err, content) => {
+        if (err) {
+          console.error(err);
+          socket.write("HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n");
+          socket.end();
+          return;
+        }
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
         socket.write(res);
-      } else {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      }
-    } else if (path === "/user-agent") {
+        socket.end();
+      });
+    } else if (requestPath.startsWith("/files/")) {
+      const directory = process.argv[3];
+      const filename = requestPath.split("/files/")[1];
+      const filePath = path.join(directory, filename);
+
+      fs.readFile(filePath, (err, content) => {
+        if (err) {
+          console.error(err);
+          socket.write("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+        } else {
+          const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n`;
+          socket.write(res);
+          socket.write(content);
+        }
+        socket.end();
+      });
+    } else if (requestPath === "/user-agent") {
+      let userAgent = '';
       req.split("\r\n").forEach((line) => {
-        if (line.includes("User-Agent")) {
-          const res = line.split(" ")[1];
-          socket.write(
-            `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n`
-          );
+        if (line.startsWith("User-Agent:")) {
+          userAgent = line.split(": ")[1];
         }
       });
-    } else if (path.startsWith("/echo/")) {
-      const res = path.split("/echo/")[1];
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n\r`
-      );
-    } else socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-    socket.end();
+      if (userAgent) {
+        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+      } else {
+        socket.write("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n");
+      }
+      socket.end();
+    } else if (requestPath.startsWith("/echo/")) {
+      const message = requestPath.split("/echo/")[1];
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`);
+      socket.end();
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+      socket.end();
+    }
+  });
+
+  socket.on("error", (err) => {
+    console.error("Socket error:", err);
   });
 });
+
 server.listen(4221, "0.0.0.0", () => {
   console.log("Server listening on port 4221");
 });
